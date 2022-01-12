fd116fe8de585d6bbc1383f05a07502bb445f84e
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-01-12 09:37:00 -0600

    fix(webapp): add color to header botton and update @proton/web-sdk

--- webapp/src/context/state.context.js
+++ webapp/src/context/state.context.js
@@ -5,7 +5,7 @@ import React, {
   useContext,
   useEffect
 } from 'react'
-import { ConnectWallet } from '@proton/web-sdk'
+import ProtonWebSDK from '@proton/web-sdk'
 import PropTypes from 'prop-types'
 
 import { sdkConfig } from '../config'
@@ -19,7 +19,7 @@ const initialValue = {
 
 const loginWallet = async (restoreSession = false) => {
   try {
-    const { link, session } = await ConnectWallet({
+    const { link, session } = await ProtonWebSDK({
       linkOptions: {
         endpoints: sdkConfig.endpoint,
         restoreSession
@@ -133,6 +133,8 @@ export const useSharedState = () => {
   const login = async () => {
     try {
       const { link, session } = await loginWallet(false)
+
+      if (!link || !session) return
 
       dispatch({
         type: 'login',
